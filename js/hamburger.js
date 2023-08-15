ac5bc4635445d198aaa1c98d2f52693d759a808c
jessinmacdon <jessinmacdon@gmail.com>
2023-08-15 20:00:21 +0200

    final commit before push

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const hamburgerButton = document.querySelector(".hamburger-button");
-	const navigationWrapper = document.querySelector(".main-navigation-wrapper");
+	const hamburgerButton = document.querySelector(".hamburger input");
+	const modal = document.querySelector(".nav-sm-modal");
+	const modalContent = document.querySelector(".nav-links-wrapper");
 
 	hamburgerButton.addEventListener("click", function () {
-		navigationWrapper.classList.toggle("hamburger-menu-open");
+		modal.classList.toggle("modal-open");
+	});
+
+	// Close modal when any click occurs within the modal content area
+	modalContent.addEventListener("click", function (event) {
+		// Check if the clicked element is a link or not
+		if (!event.target.closest("a")) {
+			modal.classList.remove("modal-open");
+		}
 	});
 });
